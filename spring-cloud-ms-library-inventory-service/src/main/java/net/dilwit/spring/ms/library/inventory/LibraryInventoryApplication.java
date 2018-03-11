9b8555c236ef1d608c94df66bc333d5e65e65eca
Dilusha Withanage <dilusha.withanage@gmail.com>
2018-03-11 20:06:10 +1100

    Added service discovery

--- spring-cloud-ms-library-inventory-service/src/main/java/net/dilwit/spring/ms/library/inventory/LibraryInventoryApplication.java
+++ spring-cloud-ms-library-inventory-service/src/main/java/net/dilwit/spring/ms/library/inventory/LibraryInventoryApplication.java
@@ -2,10 +2,10 @@ package net.dilwit.spring.ms.library.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class LibraryInventoryApplication {
 
 	public static void main(String[] args) {
