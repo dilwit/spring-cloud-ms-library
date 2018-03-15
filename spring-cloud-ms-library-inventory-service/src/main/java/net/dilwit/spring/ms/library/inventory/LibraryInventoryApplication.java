286a23e8c380787c253bfc7d8868c817e72c0523
Dilusha Withanage <dwithanage@svxgroup.com>
2018-03-15 15:56:50 +1100

    Changes to introduce feign client

--- spring-cloud-ms-library-inventory-service/src/main/java/net/dilwit/spring/ms/library/inventory/LibraryInventoryApplication.java
+++ spring-cloud-ms-library-inventory-service/src/main/java/net/dilwit/spring/ms/library/inventory/LibraryInventoryApplication.java
@@ -3,9 +3,11 @@ package net.dilwit.spring.ms.library.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class LibraryInventoryApplication {
 
 	public static void main(String[] args) {
